2411ee6b713a2c922a3b9d3c83a86e9d8bb7362b
Stephen Byrne <sdavidbyrne@gmail.com>
2023-02-11 17:11:47 +0000

    Main

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { render } from "react-dom"
-import {BrowserRouter as Router, Switch, Route, Routes, Link, Redirect} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Route, Routes, Link, Redirect, Navigate} from "react-router-dom"
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
@@ -50,7 +50,18 @@ export default class HomePage extends Component{
         return(
         <Router>
             <Routes>
-                <Route path='/' element = {this.renderHomePage()}></Route>
+                <Route
+                exact
+                path="/"
+                element={
+                    this.state.roomCode?(
+                        <Navigate to={`/room/${this.state.roomCode}`} />
+                    ):(
+                        this.renderHomePage()
+                    )
+                }
+                >
+                </Route>
                 <Route path="/join" element = {<RoomJoinPage/>}/>
                 <Route path="/create" element = {<CreateRoomPage/>}/>
                 <Route path="/room/:roomCode" element = {<Room/>}/>
--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";  
+import { useParams } from "react-router-dom";
+import { Grid, Button, Typography } from "@mui/material";  
+
 
 export default function Room() {
     
@@ -19,10 +21,37 @@ export default function Room() {
     });
        
 
-    return <div>
-            <h3>{roomCode}</h3>
-            <p>Votes: {votesToSkip}</p>
-            <p>Guest Can Pause: {String(guestCanPause)}</p>
-            <p>Host: {String(isHost)}</p>
-        </div>
+    return (
+        <Grid container spacing={1}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h4" component="h4">
+              Code: {roomCode}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h6" component="h6">
+              Votes: {votesToSkip}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h6" component="h6">
+              Guest Can Pause: {guestCanPause.toString()}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h6" component="h6">
+              Host: {isHost.toString()}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Button
+              variant="contained"
+              color="secondary"
+              
+            >
+              Leave Room
+            </Button>
+          </Grid>
+        </Grid>
+      );
 }
