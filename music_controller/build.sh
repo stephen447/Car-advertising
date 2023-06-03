084599e997cfa18719025c6c5143a8487f6db751
Stephen Byrne <sdavidbyrne@gmail.com>
2023-06-03 22:20:32 +0100

    poetry lock

--- music_controller/build.sh
+++ music_controller/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install poetry==1.2.0
 
+rm poetry.lock
+
+poetry lock
 poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
