714b57c01d1f2b07b52f18c64666c2f63e6d0e5a
Stephen Byrne <sdavidbyrne@gmail.com>
2023-06-03 22:47:20 +0100

    dotenv

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / dotenv 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR/'.env')
+#load_dotenv(BASE_DIR/'.env')
 
 
 # Quick-start development settings - unsuitable for production
